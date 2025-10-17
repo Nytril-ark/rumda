e4611eba91bd4abc95d3f009bb0eb9e6494bc08f
Hexogen <nazhim.alakkash@gmail.com>
2025-10-17 17:48:15 +0300

    added editor colorscheme and more

---
+++ nvim/lua/chadrc.lua
@@ -0,0 +1,19 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "rumda-light",
+  theme_toggle = {"rumda-light", "rumda-light"},
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+-- ayu_dark, gatekeeper, jelly beans, yoru, rxyhn
+
+return M
