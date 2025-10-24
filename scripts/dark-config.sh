26091b57ee57f6cc1050730e793b9945592ef681
Hexogen <nazhim.alakkash@gmail.com>
2025-10-24 10:06:31 +0300

    heh

--- scripts/dark-config.sh
+++ scripts/dark-config.sh
@@ -2,7 +2,7 @@
 /home/${USER}/.config/rumda/scripts/wall.sh dark && \
 sleep 0.5
 
-# CHANGED: launch quickshell directly (no outer subshell) and grab its PID
+# launch quickshell directly and grab its PID
 quickshell -p /home/${USER}/.config/rumda/dark-config/quickshell/shell.qml & 
 NEW_PID=$!
 disown $NEW_PID
@@ -10,6 +10,6 @@ disown $NEW_PID
 # wait a bit to make sure it’s running
 sleep 1
 
-# CHANGED: only kill *other* quickshells, not this one
+# only kill *other* quickshells, not this one
 pgrep -af "quickshell -p" | awk -v pid="$NEW_PID" '$1 != pid { print $1 }' | xargs -r kill
 
