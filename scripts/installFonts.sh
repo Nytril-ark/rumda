fe2bc544926cb4f3d07181c5bb9f90d71bfc452d
Nytril-ark <nazhim.alakkash@gmail.com>
2026-03-31 19:24:15 +0200

    Update installFonts.sh

--- scripts/installFonts.sh
+++ scripts/installFonts.sh
@@ -3,7 +3,7 @@
 echo "Copying fonts to ~/.local/share/fonts."
 echo ""
 mkdir -p ~/.local/share/fonts
-sudo cp -r /home/$USER/.config/rumda/common/fonts/* ~/.local/share/fonts
+cp -r /home/$USER/.config/rumda/common/fonts/* ~/.local/share/fonts
 
 fc-cache -fv > /dev/null
 
